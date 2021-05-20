68b275e9607350c289bf380358f582d90dfe7952
github username <github email address>
2021-05-20 00:17:49 -0400

    commit tree b98bff8d3f71f4f4680b25ec0477ecfdb3f64725

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,8 @@
 // Code SimplerComponent Here
+import { render } from 'enzyme'
+import React, {Component} from 'react'
+
+export default function SimplerComponent(props) {
+    debugger
+       return <div onClick={props.handleClick}>I am just happy</div>
+}
